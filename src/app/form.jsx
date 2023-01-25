cfa02183ced1d8dfb09ceea615ddebcf1bfba261
Pedro Dominguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2023-01-25 19:00:02 +0100

    add form validations

--- src/app/form.jsx
+++ src/app/form.jsx
@@ -1,74 +1,90 @@
 "use client";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+import * as yup from "yup";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { useForm } from "react-hook-form";
+
 import styles from "./page.module.css";
 
+const schema = yup
+  .object({
+    url: yup
+      .string()
+      .required("La url es requerida")
+      .url("No es una url válida"),
+    size: yup.string().required("La talla es requerida"),
+  })
+  .required();
+
 export default function Form() {
-  const [formData, setFormData] = useState({
-    store: "",
-    url: "",
-    size: "",
+  const [response, setResponse] = useState({
+    type: false,
+    message: "",
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
-  const handleInput = (e) => {
-    const fieldName = e.target.name;
-    const fieldValue = e.target.value;
+  useEffect(() => {
+    if (isDirty) {
+      setResponse({
+        isOk: false,
+        message: "",
+      });
+    }
+  }, [isDirty]);
 
-    setFormData((prevState) => ({
-      ...prevState,
-      [fieldName]: fieldValue,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    // We don't want the page to refresh
-    e.preventDefault();
-    console.log("formData", formData);
-
+  const onSubmit = (data) => {
     fetch("api/sendData", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
       },
-      body: JSON.stringify(formData),
-    })
-      .then((res) => res.json())
-      .then((data) =>
-        setFormData({
-          store: "",
-          url: "",
-          size: "",
-        })
-      )
-      .catch((err) => console.log("err", err));
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        reset();
+        setResponse({
+          isOk: true,
+          message: "El producto fue dado de alta correctamente.",
+        });
+      } else {
+        setResponse({
+          isOk: false,
+          message: "Error al dar de alta el producto, intentelo de nuevo.",
+        });
+      }
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="store">Choose a store:</label>
-      <select name="store" id="store" onChange={handleInput}>
-        <option>select</option>
-        <option value="zara">Zara</option>
-        <option value="stradivarius">Stradivarius</option>
-      </select>
-      <label htmlFor="url">Choose a url:</label>
-      <input
-        name="url"
-        id="url"
-        type="text"
-        placeholder="Enter the url"
-        className={styles.urlInput}
-        onChange={handleInput}
-      />
-      <label htmlFor="size">Choose a size:</label>
-      <input
-        type="text"
-        name="size"
-        placeholder="Enter the size"
-        id="size"
-        onChange={handleInput}
-      />
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {console.log("isDirty", isDirty)}
+      <div className={styles.customInput}>
+        <label htmlFor="url">Url del producto:</label>
+        <input id="url" {...register("url")} />
+        <span>{errors.url?.message}</span>
+      </div>
+      <div className={styles.customInput}>
+        <label htmlFor="size">Talla:</label>
+        <input id="size" {...register("size")} />
+        <span>{errors.size?.message}</span>
+      </div>
       <button type="submit">Check</button>
+      {response.message && (
+        <p className={response.isOk ? styles.ok : styles.error}>
+          {response.message}
+        </p>
+      )}
     </form>
   );
 }
