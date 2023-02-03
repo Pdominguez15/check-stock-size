f8d50f53e018be27b4fe53bb00db937c4c9d735b
Pedro Dominguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2023-02-03 12:08:33 +0100

    add analytics

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
 import Form from "./form/form";
+
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Analytics />
       <div className={styles.description}>
         <h1>Comprobar stock</h1>
       </div>
