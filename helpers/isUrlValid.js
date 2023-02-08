980fbc11dd563b512cd6bcc2a22fc9014f49b8b2
Pedro Dominguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2023-02-08 10:48:48 +0100

    add bershka and pullandbear

--- helpers/isUrlValid.js
+++ helpers/isUrlValid.js
@@ -1,6 +1,13 @@
+import { isBershkaUrl } from "./isBershkaUrl";
+import { isPullAndBearUrl } from "./isPullAndBearUrl";
 import { isStradivariusUrl } from "./isStradivariusUrl";
 import { isZaraUrl } from "./isZaraUrl";
 
 export const isUrlValid = (url) => {
-  return isZaraUrl(url) || isStradivariusUrl(url);
+  return (
+    isZaraUrl(url) ||
+    isStradivariusUrl(url) ||
+    isPullAndBearUrl(url) ||
+    isBershkaUrl(url)
+  );
 };
