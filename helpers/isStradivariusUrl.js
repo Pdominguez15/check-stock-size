0ae9e6abd4b31687f7d28472c7c8ee09f649b24d
Pedro Domínguez Aguilera <p.dominguezaguilera@ext.douglas.eu>
2024-01-07 16:27:33 +0100

    Fix stradivarius url

--- helpers/isStradivariusUrl.js
+++ helpers/isStradivariusUrl.js
@@ -1,5 +1,7 @@
 export const isStradivariusUrl = (url) => {
-  const regexStradivarius = new RegExp(/https:\/\/www.stradivarius.com\/es\//);
+  const regexStradivarius = new RegExp(
+    /https:\/\/www.stradivarius.com\/[eE][sS]\//
+  );
 
   return regexStradivarius.test(url);
 };
